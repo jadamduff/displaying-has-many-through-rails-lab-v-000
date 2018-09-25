27c3783d1f1df90071135c0627af6dc44eadae16
Adam Duff <j.adam.duff@gmail.com>
2018-09-25 05:55:15 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180925055258) do
-
-  create_table "appointments", force: :cascade do |t|
-    t.datetime "appointment_datetime"
-    t.integer  "patient_id"
-    t.integer  "doctor_id"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-  end
+ActiveRecord::Schema.define(version: 20180925055452) do
 
   create_table "doctors", force: :cascade do |t|
     t.string   "name"
